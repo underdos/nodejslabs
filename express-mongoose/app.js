299e5ddf83e7bde80a234927773b957b06bb6881
Kusnadi <kus.underdos@gmail.com>
2018-02-12 23:11:53 +0700

    update app.js

--- express-mongoose/app.js
+++ express-mongoose/app.js
@@ -9,3 +9,32 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("we're connected!")
 });
+
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+  username: String,
+  password: String,
+  name: String,
+  email: String,
+  phone: String
+})
+
+var User = mongoose.model('User', userSchema)
+
+var kusnadi = new User({
+  username: 'kusnadi',
+  password: 'admin',
+  name: 'Kusnadi',
+  email: 'kus.underdos@gmail.com',
+  phone: '085780621341'
+})
+
+kusnadi.save(function(err){
+  if (err) return handleError(err);
+
+  console.log('data saved');
+})
+// app.get('/', function(req, res){
+//   var kusnadi
+// })
